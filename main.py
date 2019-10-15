c99255aba465746bf23ae36aa52d634e4b47b6b4
mark104478 <49283068+mark104478@users.noreply.github.com>
2019-10-15 15:00:44 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+import sys, traceback
+import discord
+from discord.ext import commands
+
+import settings
+
+
+initial_extensions = ["modules.image", "modules.fortnite"]
+TOKEN = os.getenv("TOKEN")
+bot = commands.Bot(command_prefix="-")
+
+if __name__ == "__main__":
+    for extension in initial_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print(f"Failed to load extension {extension}.", file=sys.stderr)
+            traceback.print_exc()
+
+
+@bot.event
+async def on_ready():
+    print("Logged in as")
+    print(bot.user.name)
+    print(bot.user.id)
+    print("-----------")
+
+
+@bot.command()
+async def greet(ctx):
+    await ctx.send(":wave: Hello there!")
+
+
+bot.run(TOKEN)
